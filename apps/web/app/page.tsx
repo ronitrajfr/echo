e8e9aa8abc1a2f2a440abd43f75c3731224f6718
ronitrajfr <ronitrajofficial7@gmail.com>
2025-12-26 18:18:53 +0530

    feat: clerk init

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,19 +3,35 @@
 import { Button } from "@workspace/ui/components/button";
 import { useMutation, useQuery } from "convex/react";
 import { api } from "@workspace/backend/_generated/api";
+import { Authenticated, Unauthenticated } from "convex/react";
+import { SignInButton, UserButton } from "@clerk/nextjs";
+
 export default function Page() {
   const users = useQuery(api.users.getMany);
 
   const addUser = useMutation(api.users.add);
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello apps/web</h1>
-        <Button size="sm" onClick={() => addUser()}>
-          ADD
-        </Button>
-        {JSON.stringify(users)}
-      </div>
-    </div>
+    <>
+      <Authenticated>
+        <div className="flex items-center justify-center min-h-svh">
+          <div className="flex flex-col items-center justify-center gap-4">
+            <h1 className="text-2xl font-bold">Hello apps/web</h1>
+            <UserButton />
+            <Button size="sm" onClick={() => addUser()}>
+              ADD
+            </Button>
+            {JSON.stringify(users)}
+          </div>
+        </div>
+      </Authenticated>
+      <Unauthenticated>
+        <div className="flex items-center justify-center min-h-svh">
+          <div className="flex flex-col items-center justify-center gap-4">
+            <h1 className="text-2xl font-bold">Please sign in to continue</h1>
+            <SignInButton />
+          </div>
+        </div>
+      </Unauthenticated>
+    </>
   );
 }
