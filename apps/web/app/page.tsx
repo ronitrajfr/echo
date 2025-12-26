8e5f30fecd25109ba1ebd27514c47660f1bf3b71
ronitrajfr <ronitrajofficial7@gmail.com>
2025-12-26 12:36:27 +0530

    feat: convex message

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,20 @@
-import { Button } from "@workspace/ui/components/button";
+"use client";
 
+import { Button } from "@workspace/ui/components/button";
+import { useMutation, useQuery } from "convex/react";
+import { api } from "@workspace/backend/_generated/api";
 export default function Page() {
+  const users = useQuery(api.users.getMany);
+
+  const addUser = useMutation(api.users.add);
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">Hello apps/web</h1>
-        <Button size="sm">Button</Button>
+        <Button size="sm" onClick={() => addUser()}>
+          ADD
+        </Button>
+        {JSON.stringify(users)}
       </div>
     </div>
   );
